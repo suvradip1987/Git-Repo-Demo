8c266feb98471f98e94b3ceaf41c1ceaa97bbe62
Suvradip <suvradip@yahoo.co.in>
2023-10-09 18:53:19 +0530

    modified program.cs in feature branch

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@ p.s.v.m()
     This line is added by Developer B. 
 }
 
+public int Sub(int num1, int num2)
+{
+    retrun num1- num2;
+}
+
+private static int SumOfTwoNumbers(int num1, int num2)
+{
+    return num1+num2;
+}
