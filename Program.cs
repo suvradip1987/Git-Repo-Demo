c4aec78a40743a07c4eb81d1488e832a470d77b1
Suvradip <suvradip@yahoo.co.in>
2023-10-11 12:19:44 +0530

    Added Sub and Sum functionality

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 p.s.v.m()
 {
-    This line is added by Developer B. 
+    This line is added by Developer B.
     quick fix provided for bug..
+    some changes made... 
+}
+
+
+private int SumOfTwoNumbers(int num1, int num2)
+{
+    return num1+num2;
+}
+
+private int SubOfTwoNumbers(int num1, int num2)
+{
+    return num1-num2;
 }
 
